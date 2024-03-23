a8ba87fdf92626df8f416e2dff18a0b732652e8a
Princebrar <princebrar104@gmail.com>
2024-03-23 17:19:12 -0600

    Lab-5

--- App.js
+++ App.js
@@ -3,37 +3,22 @@
  *
  * @format
  */
-"use client";
 import React from 'react';
-import TodoList from './Component/ToDoList.js';
-import TodoForm from './Component/ToDoForm.js';
-import {   SafeAreaView,StatusBar} from 'react-native';
-import { useState} from 'react';
+import Home from './Screens/Homescreen.jsx';
+import About from './Screens/Aboutscreen.jsx';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+const Stack = createStackNavigator();
 function App() {
-    const [tasks, setTasks] = useState([
-        'Do laundry',
-        'Go to gym',
-        'Walk dog'
-      ]);
-    const addTask = (task) => {
-        for(let i=0;i<tasks.length;i++){
-            if(tasks[i]===task){
-                alert("Task Already Exists");
-                return;
-            }
-        }
-        setTasks([...tasks, task]);
-    }
     return (
-        
-        <SafeAreaView>
-            <TodoList items={tasks}/>
-            <TodoForm addTask={addTask}/>
-            <StatusBar style="auto" />
-        </SafeAreaView>
-        
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="About" component={About} />
+        </Stack.Navigator>
+      </NavigationContainer>
     );
-}
+  }
 
 
 
